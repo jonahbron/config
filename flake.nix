061cce6ac07f4d4e5798778e6bca8a734b7832a1
Jonah Dahlquist <hi@jonah.name>
2022-12-14 19:40:04 -0800

    initial attempt

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "A very basic flake";
+  
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/";
+    rock5b-nixos.url = "github:aciceri/rock5b-nixos";
+  };
+
+  outputs = { self, nixpkgs, rock5b-nixos }:
+  let
+    lib = nixpkgs.lib;
+  in {
+    nixosConfigurations = {
+      box = lib.nixosSystem {
+        system = "aarch64-linux";
+        modules = [
+	  rock5b-nixos.nixosModules.kernel
+	];
+      };
+    };
+  };
+}
