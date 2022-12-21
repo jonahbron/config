2882a1a53ec64f857fa7b504a52a3a55924bd86f
Jonah Dahlquist <hi@jonah.name>
2022-12-21 15:19:33 -0800

    Added cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,11 @@
 {
   description = "A very basic flake";
   
+  nixConfig = {
+    extra-substituters = ["https://rock5b-nixos.cachix.org"];
+    extra-trusted-public-keys = ["rock5b-nixos.cachix.org-1:bXHDewFS0d8pT90A+/YZan/3SjcyuPZ/QRgRSuhSPnA="];
+  };
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5";
     rock5b-nixos.url = "github:aciceri/rock5b-nixos";
