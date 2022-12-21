9c88452a5c86baddf341448ca455fc69ebe18cc0
Jonah Dahlquist <hi@jonah.name>
2022-12-21 11:13:14 -0800

    Pinned nixpkgs version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
   
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/";
+    nixpkgs.url = "github:nixos/nixpkgs/f2537a505d45c31fe5d9c27ea9829b6f4c4e6ac5";
     rock5b-nixos.url = "github:aciceri/rock5b-nixos";
   };
 
